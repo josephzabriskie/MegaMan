760904a63415c8503e341486d5b0e407ea1a7ab8
Joe Zabriskie <josephzabriskie@gmail.com>
2016-12-23 23:12:53 -0700

    Added start menu and replaced built in gravity with code gravity

---
+++ MegaMan/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public Transform mainMenu, optionsMenu;
+
+	public void LoadScene(string name)
+	{
+		Application.LoadLevel(name);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	public void OptionsMenu(bool clicked)
+	{
+		if (clicked == true) 
+		{
+			optionsMenu.gameObject.SetActive (true);
+			mainMenu.gameObject.SetActive (false);
+		}
+		else
+		{
+			mainMenu.gameObject.SetActive (true);
+			optionsMenu.gameObject.SetActive (false);
+		}
+	}
+}
